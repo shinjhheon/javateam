1eb54fe1b856922fd4d857f03f9d8b6791c5e178
shinjhheon <113599642+shinjhheon@users.noreply.github.com>
2022-12-09 13:32:46 +0900

    Add files via upload

---
+++ src/cBbs.java
@@ -0,0 +1,50 @@
+package cbbs;
+
+public class cBbs {
+
+	private int cbbsID;
+	private String cbbsTitle;
+	private String userID;
+	private String cbbsDate;
+	private String cbbsContent;
+	private int cbbsAvailable;	
+	public int getcbbsID() {
+		return cbbsID;
+	}
+	public void setCbbsID(int bbsID) {
+		this.cbbsID = bbsID;
+	}
+	public String getCbbsTitle() {
+		return cbbsTitle;
+	}
+	public void setCbbsTitle(String cbbsTitle) {
+		this.cbbsTitle = cbbsTitle;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getCbbsDate() {
+		return cbbsDate;
+	}
+	public void setCbbsDate(String cbbsDate) {
+		this.cbbsDate = cbbsDate;
+	}
+	public String getCbbsContent() {
+		return cbbsContent;
+	}
+	public void setCbbsContent(String cbbsContent) {
+		this.cbbsContent = cbbsContent;
+	}
+	public int getCbbsAvailable() {
+		return cbbsAvailable;
+	}
+	public void setCbbsAvailable(int cbbsAvailable) {
+		this.cbbsAvailable = cbbsAvailable;
+	}
+
+	
+	
+}
