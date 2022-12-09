f88fc91edd386d1f48556d2f5c23c2259e1773b1
shinjhheon <113599642+shinjhheon@users.noreply.github.com>
2022-12-09 13:33:24 +0900

    Add files via upload

---
+++ src/hBbs.java
@@ -0,0 +1,48 @@
+package hbbs;
+
+public class hBbs {
+	private int hbbsID;
+	private String hbbsTitle;
+	private String userID;
+	private String hbbsDate;
+	private String hbbsContent;
+	private int hbbsAvailable;
+	public int getHbbsID() {
+		return hbbsID;
+	}
+	public void setHbbsID(int hbbsID) {
+		this.hbbsID = hbbsID;
+	}
+	public String getHbbsTitle() {
+		return hbbsTitle;
+	}
+	public void setHbbsTitle(String hbbsTitle) {
+		this.hbbsTitle = hbbsTitle;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getHbbsDate() {
+		return hbbsDate;
+	}
+	public void setHbbsDate(String hbbsDate) {
+		this.hbbsDate = hbbsDate;
+	}
+	public String getHbbsContent() {
+		return hbbsContent;
+	}
+	public void setHbbsContent(String hbbsContent) {
+		this.hbbsContent = hbbsContent;
+	}
+	public int getHbbsAvailable() {
+		return hbbsAvailable;
+	}
+	public void setHbbsAvailable(int hbbsAvailable) {
+		this.hbbsAvailable = hbbsAvailable;
+	}
+	
+	
+}
