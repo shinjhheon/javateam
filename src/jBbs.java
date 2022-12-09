51afbc7579fe218cfc6cad4856c72c61a0f778bc
shinjhheon <113599642+shinjhheon@users.noreply.github.com>
2022-12-09 13:33:55 +0900

    Add files via upload

---
+++ src/jBbs.java
@@ -0,0 +1,48 @@
+package jbbs;
+
+public class jBbs {
+	private int jbbsID;
+	private String jbbsTitle;
+	private String userID;
+	private String jbbsDate;
+	private String jbbsContent;
+	private int jbbsAvailable;
+	public int getJbbsID() {
+		return jbbsID;
+	}
+	public void setJbbsID(int jbbsID) {
+		this.jbbsID = jbbsID;
+	}
+	public String getJbbsTitle() {
+		return jbbsTitle;
+	}
+	public void setJbbsTitle(String jbbsTitle) {
+		this.jbbsTitle = jbbsTitle;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getJbbsDate() {
+		return jbbsDate;
+	}
+	public void setJbbsDate(String jbbsDate) {
+		this.jbbsDate = jbbsDate;
+	}
+	public String getJbbsContent() {
+		return jbbsContent;
+	}
+	public void setJbbsContent(String jbbsContent) {
+		this.jbbsContent = jbbsContent;
+	}
+	public int getJbbsAvailable() {
+		return jbbsAvailable;
+	}
+	public void setJbbsAvailable(int jbbsAvailable) {
+		this.jbbsAvailable = jbbsAvailable;
+	}
+	
+	
+}
